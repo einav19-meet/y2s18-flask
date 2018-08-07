b52d529e3ce2bc202c481a82377286e3d2bcb84c
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:12:16 +0300

    suicide

--- exercises/app.py
+++ exercises/app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    players = ["salwa", "tamer", "mohammad"]
+    player1="einav"
+    return render_template("index.html",player1=player1,players=players,likes_same_sport=True)
 
-
+ 
 if __name__ == '__main__':
     app.run(debug=True)
